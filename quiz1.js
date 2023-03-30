618977844eaa06264a630e80ab62d1fc3d3d9677
Bazli <bazlibinnazri35@gmail.com>
2023-03-30 15:58:46 +0800

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,47 @@
+// let message = "benr"
+// console.log(message, typeof message)
+
+// message = 123
+// console.log(message, typeof message)
+
+
+
+// let str = "1.258"
+// let num = 1
+
+// let total = parseFloat(str) + num
+// console.log(total, typeof total)
+
+// let data = ["benr",3532, true]
+
+// console.log(data[0])
+// console.log(data[1])
+// console.log(data[2])
+// console.log(data[3])
+
+// let user = {
+//     name:"bazli",
+//     age:23,
+//     faculty:"FKEKK"
+// }
+
+// console.log(user.name)
+// console.log(user.age)
+
+let message = [
+    {
+        name: "usr1", 
+        date: "1/4/2023",
+        msg: "nkbukapuasangnayg",
+        like: 2001
+    },
+    {
+        name: "usr2",
+        date: "2/4/2023",
+        msg: "tpiayanghangit"
+        like: 3020
+    },
+]
+console.log(message[1].msg)
+
+]
